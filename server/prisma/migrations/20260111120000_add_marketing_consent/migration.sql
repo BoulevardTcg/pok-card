2c6cccda3bc68762c595bc8c7f651271b47c3c76
issa <morais_v@etna-alternance.net>
2026-01-11 16:26:47 +0100

    fix: add all missing GDPR fields to migration (analytics, privacy, deletion)

--- server/prisma/migrations/20260111120000_add_marketing_consent/migration.sql
+++ server/prisma/migrations/20260111120000_add_marketing_consent/migration.sql
@@ -1,3 +1,9 @@
--- AlterTable
+-- AlterTable: Add GDPR consent and deletion fields
 ALTER TABLE "users" ADD COLUMN "marketingConsent" BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE "users" ADD COLUMN "marketingConsentAt" TIMESTAMP(3);
+ALTER TABLE "users" ADD COLUMN "analyticsConsent" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "users" ADD COLUMN "analyticsConsentAt" TIMESTAMP(3);
+ALTER TABLE "users" ADD COLUMN "privacyPolicyVersion" TEXT;
+ALTER TABLE "users" ADD COLUMN "privacyAcceptedAt" TIMESTAMP(3);
+ALTER TABLE "users" ADD COLUMN "deletionRequestedAt" TIMESTAMP(3);
+ALTER TABLE "users" ADD COLUMN "deletionScheduledAt" TIMESTAMP(3);
